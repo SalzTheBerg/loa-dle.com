d06f4f72082c9fffdddbcf45577759e456de1bf7
SalzTheBerg <silas.hittler@gmx.de>
2025-03-13 15:56:10 +0100

    Still suuper buggy but ability loading starts to work

--- ability.js
+++ ability.js
@@ -1,147 +1,51 @@
-var characterList = [];
-var availableCharacterNames = [];
+var abilityList = [];
+var availableClasses = [];
+var classList = [];
+const rotationAngles = [90, 180, 270];
+const dailyRotation = rotationAngles[Math.floor(Math.random()*rotationAngles.length)];
 
-fetch("characterList.json")
+fetch("abilityList.json")
     .then(response => response.json())
     .then(data => {
-        characterList = data;
-        availableCharacterNames = characterList.map(character => character.name);
-        availableCharacterNames.sort();
+        abilityList = data;
+        availableClasses = Object.keys(data);
+        classList = Object.keys(data);
+        availableClasses.sort();
+        console.log(abilityList);
+        console.log(classList);
+
+        loadImg();
     })
     .catch(error => console.error("Error loading character data:", error));
 
-var guessTable = document.getElementById("guess_table");
-var inputField = document.getElementById("input_guess");
-var usedCharacters = [];
+function loadImg() {
+    const dailyClass = classList[Math.floor(Math.random()*classList.length)];
+    const dailySkill = abilityList[dailyClass].abilities[Math.floor(Math.random()*abilityList[dailyClass].abilities.length)];
 
-const correctColor = "rgb(96, 220, 0)";
-const partialMatchColor = "rgb(225, 225, 0)";
-const wrongColor = "rgb(238, 42, 0)";
+    var guessImage = document.getElementById("guess_image");
+    guessImage.innerHTML = '<img src="Abilities/' + dailyClass + '/' + dailySkill + '.webp">';
+    guessImage.style.filter = "grayscale(100%)"
+    guessImage.style.transform = "rotate(" + dailyRotation + "deg)";
+};
 
-var characterToGuess = null;
+document.getElementById("grayscale_checkbox").addEventListener("change", function() {
+    const grayscaleCheckbox = document.getElementById("grayscale_checkbox");
+    var guessImage = document.getElementById("guess_image");
 
-function getDailyCharacter() {
-    const today = new Date().toISOString().split('T')[0];
-    const hash = fnv1aHash(today);
-    return characterList[hash % characterList.length];
-}
-
-function fnv1aHash(today) {
-    let hash = 0x811c9dc5;
-    for (let i = 0; i < today.length; i++) {
-        hash ^= today.charCodeAt(i);
-        hash = (hash * 0x01000193) >>> 0;
-    }
-    console.log(hash);
-    return hash;
-}
-
-function createRow(indexOfChar) {
-    characterToGuess = getDailyCharacter();
-
-    var table = document.getElementById("guess_table")
-    var newRow = table.insertRow(1);
-    let character = characterList[indexOfChar];
-
-    let attributes = ["name", "gender", "race", "region", "occupation", "affinity", "status"]
-
-    let newCell = newRow.insertCell(0);
-
-    let image = character.name + ".webp";
-
-    newCell.style.backgroundImage = "url('Icons/" + image + "')";
-
-    for (let i = 1; i < 7; i++) {
-        let newCell = newRow.insertCell(i);
-
-        let attributeName = attributes[i];
-
-        if(character[attributeName] === characterToGuess[attributeName] || JSON.stringify(character[attributeName]) === JSON.stringify(characterToGuess[attributeName])) {
-            newCell.style.backgroundColor = correctColor;
-        } else if(character[attributeName].constructor === Array && character[attributeName].some(item => characterToGuess[attributeName].includes(item))) {
-            newCell.style.backgroundColor = partialMatchColor;
-        } else {
-            newCell.style.backgroundColor = wrongColor;
-        }
-
-        if(character[attributeName].constructor === Array){
-            newCell.innerHTML = character[attributeName].join(",<br>");
-        } else {
-            newCell.innerHTML = character[attributeName];
-        }
-    }
-}
-
-function getInput() {
-    //clearing suggestion container
-    let suggestionsContainer = document.getElementById("suggestions");
-    suggestionsContainer.innerHTML = '';
-
-    let inputContent = document.getElementById("input_guess");
-    let value = inputContent.value.toLowerCase();
-
-    for (let i = 0; i < characterList.length; i++) {
-        let character = characterList[i];
-        if (value === character.name.toLowerCase() && !usedCharacters.includes(value)) {
-            if (guessTable.style.display === "none") {
-                guessTable.style.display = "table";
-            }
-            createRow(i);
-            usedCharacters.push(character.name.toLowerCase());
-            break;
-        }
-    }
-
-    inputField.value = "";
-}
-
-var inputField = document.getElementById("input_guess");
-
-inputField.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-
-        let query = this.value.toLowerCase();
-
-        if (query === '') {
-            return;
-        }
-
-        let suggestions = availableCharacterNames.filter(name => name.toLowerCase().startsWith(query));
-        removeItem(availableCharacterNames, suggestions[0]);
-
-        if (suggestions.length > 0) {
-            this.value = suggestions[0];
-        }
-        document.getElementById("input_submit").click();
+    if (grayscaleCheckbox.checked) {
+        guessImage.style.filter = "grayscale(100%)"
+    } else {
+        guessImage.style.filter = "grayscale(0%)"
     }
 });
 
-inputField.addEventListener("input", function () {
-    let query = this.value.toLowerCase();
-    let suggestions = availableCharacterNames.filter(name => name.toLowerCase().startsWith(query));
+document.getElementById("rotation_checkbox").addEventListener("change", function() {
+    const grayscaleCheckbox = document.getElementById("rotation_checkbox");
+    var guessImage = document.getElementById("guess_image");
 
-    if (query === '') {
-        document.getElementById("suggestions").innerHTML = '';
-        return;
+    if (grayscaleCheckbox.checked) {
+        guessImage.style.transform = "rotate(" + dailyRotation + "deg)";
+    } else {
+        guessImage.style.transform = "rotate(" + 0 + "deg)";
     }
-
-    let suggestionsContainer = document.getElementById("suggestions");
-    suggestionsContainer.innerHTML = '';
-
-
-    suggestions.forEach(suggestion => {
-        let suggestionItem = document.createElement("div");
-        suggestionItem.classList.add('suggestion-item')
-        suggestionItem.innerHTML = '<img src="Icons/' + suggestion + '.webp">' + suggestion;
-        suggestionsContainer.appendChild(suggestionItem);
-    });
 });
-
-function removeItem(array, itemToRemove) {
-    const index = array.indexOf(itemToRemove);
-
-    if (index !== -1) {
-        array.splice(index, 1);
-    }
-}
