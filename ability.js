6f05a4259a4ce400ff08361270beb45f8bf88bbe
SalzTheBerg <silas.hittler@gmx.de>
2025-03-18 15:15:38 +0100

    Fixed image rotating div

--- ability.js
+++ ability.js
@@ -1,8 +1,14 @@
-var abilityList = [];
-var availableClasses = [];
-var classList = [];
+let abilityList = [];
+let availableClasses = [];
+let classList = [];
 const rotationAngles = [90, 180, 270];
 const dailyRotation = rotationAngles[Math.floor(Math.random()*rotationAngles.length)];
+
+const imageDiv = document.getElementById("guess_image");
+
+const grayscaleCheckbox = document.getElementById("grayscale_checkbox");
+
+const rotationCheckbox = document.getElementById("rotation_checkbox");
 
 fetch("abilityList.json")
     .then(response => response.json())
@@ -19,33 +25,45 @@ fetch("abilityList.json")
     .catch(error => console.error("Error loading character data:", error));
 
 function loadImg() {
+    console.log(classList);
     const dailyClass = classList[Math.floor(Math.random()*classList.length)];
+    console.log(dailyClass);
+    console.log(abilityList);
     const dailySkill = abilityList[dailyClass].abilities[Math.floor(Math.random()*abilityList[dailyClass].abilities.length)];
+    console.log(dailySkill);
 
-    var guessImage = document.getElementById("guess_image");
-    guessImage.innerHTML = '<img src="Abilities/' + dailyClass + '/' + dailySkill + '.webp">';
-    guessImage.style.filter = "grayscale(100%)"
-    guessImage.style.transform = "rotate(" + dailyRotation + "deg)";
+    imageDiv.innerHTML = '<img src="Abilities/' + dailyClass + '/' + dailySkill + '.webp" id="dailySkill">';
+    applyFilters();
+};
+
+function applyFilters() {
+    const dailyImage = document.getElementById("dailySkill");
+    if (grayscaleCheckbox.checked) {
+        dailyImage.style.filter = "grayscale(100%)";
+    } else {
+        dailyImage.style.filter = "grayscale(0%)";
+    }
+    if (rotationCheckbox.checked) {
+        dailyImage.style.transform = "rotate(" + dailyRotation + "deg)";
+    } else {
+        dailyImage.style.transform = "rotate(" + 0 + "deg)";
+    }
 };
 
 document.getElementById("grayscale_checkbox").addEventListener("change", function() {
-    const grayscaleCheckbox = document.getElementById("grayscale_checkbox");
-    var guessImage = document.getElementById("guess_image");
-
+    const dailyImage = document.getElementById("dailySkill");
     if (grayscaleCheckbox.checked) {
-        guessImage.style.filter = "grayscale(100%)"
+        dailyImage.style.filter = "grayscale(100%)";
     } else {
-        guessImage.style.filter = "grayscale(0%)"
+        dailyImage.style.filter = "grayscale(0%)";
     }
 });
 
 document.getElementById("rotation_checkbox").addEventListener("change", function() {
-    const grayscaleCheckbox = document.getElementById("rotation_checkbox");
-    var guessImage = document.getElementById("guess_image");
-
-    if (grayscaleCheckbox.checked) {
-        guessImage.style.transform = "rotate(" + dailyRotation + "deg)";
+    const dailyImage = document.getElementById("dailySkill");
+    if (rotationCheckbox.checked) {
+        dailyImage.style.transform = "rotate(" + dailyRotation + "deg)";
     } else {
-        guessImage.style.transform = "rotate(" + 0 + "deg)";
+        dailyImage.style.transform = "rotate(" + 0 + "deg)";
     }
 });
