88c2a011a18b6d73a3cd71342d0c32b9047dcc30
SalzTheBerg <silas.hittler@gmx.de>
2025-03-24 17:47:11 +0100

    added function for gender unlock to be recognized correctly

--- ability.js
+++ ability.js
@@ -5,6 +5,11 @@ let availableClasses = [];
 const rotationAngles = [90, 180, 270];
 const dailyRotation = rotationAngles[Math.floor(Math.random()*rotationAngles.length)];
 
+const genderUnlockClasses = ["Berserker", "Slayer"];
+let genderUnlockGroups = [];
+let alternateClass;
+let alternateSkill;
+
 const imageDiv = document.getElementById("guess_image");
 
 const grayscaleCheckbox = document.getElementById("grayscale_checkbox");
@@ -19,6 +24,7 @@ const inputDiv = document.getElementById("input_div");
 const skillInput = document.getElementById("skill_input_div");
 const skillGuess = document.getElementById("skill_guess");
 const skillSubmit = document.getElementById("skill_submit");
+const suggestionsContainer = document.getElementById("suggestions");
 
 const correctColor = "rgb(96, 220, 0)";
 const wrongColor = "rgb(238, 42, 0)";
@@ -30,19 +36,23 @@ let dailyImage;
 let classGuessed = false;
 
 //loading the abilityList.json and saving data in abilityList array and class names in classList and availableClasses, calls the loadImg(); function after finishing
-fetch("abilityList.json")
-    .then(response => response.json())
-    .then(data => {
-        abilityList = data;
-        classList = Object.keys(data);
-        availableClasses = Object.keys(data);
+Promise.all([
+    fetch("abilityList.json").then(response => response.json()),
+    fetch("genderUnlock.json").then(response => response.json())
+])
+    .then(([abilityListData, genderUnlockData]) => {
+        abilityList = abilityListData;
+        classList = Object.keys(abilityList);
+        availableClasses = Object.keys(abilityList);
         availableClasses.sort();
         console.log(abilityList);
         console.log(classList);
 
+        genderUnlockGroups = genderUnlockData;
+        console.log(genderUnlockGroups);
         loadImg();
     })
-    .catch(error => console.error("Error loading character data:", error));
+    .catch(error => console.error("Error loading data:", error));
 
 //loads the daily image randomly and adding it to the DOM, calls the applyFilters(); method when done
 function loadImg() {
@@ -91,7 +101,6 @@ document.getElementById("rotation_checkbox").addEventListener("change", function
 //called upon submit or enter -> reads input and adds tablerow (calling createRow(); function) when input == className
 function getInput() {
     //clearing suggestion container
-    let suggestionsContainer = document.getElementById("suggestions");
     suggestionsContainer.innerHTML = '';
     let value = inputContent.value.toLowerCase();
 
@@ -122,6 +131,8 @@ function createRow(indexOfChar) {
 
     if (classList[indexOfChar] === dailyClass) {
         classGuessed = true;
+        alternateClass = dailyClass;
+        alternateSkill = dailySkill;
         newCell.style.backgroundColor = correctColor;
         inputContent.placeholder = "Enter skill name..."
         dailyImage.style.filter = "grayscale(0%)";
@@ -133,9 +144,56 @@ function createRow(indexOfChar) {
         const node2 = document.createTextNode("Can you also guess the ability name?");
         head.appendChild(node1);
         para.appendChild(node2);
-        inputDiv.appendChild(head);
-        inputDiv.appendChild(para);
+        suggestionsContainer.appendChild(head);
+        suggestionsContainer.appendChild(para);
+    } else if (checkForGenderUnlock()) {
+        classGuessed = true;
+        newCell.style.backgroundColor = correctColor;
+        inputContent.placeholder = "Enter skill name..."
+        dailyImage.style.filter = "grayscale(0%)";
+        dailyImage.style.transform = "rotate(" + 0 + "deg)";
+        const para = document.createElement("p");
+        const para2 = document.createElement("p");
+        const head = document.createElement("h2");
+        const node1 = document.createTextNode("Congratulations!");
+        const node2 = document.createTextNode("The daily class was " + dailyClass + " But since this is also a skill for " + alternateClass + " it counts!");
+        const node3 = document.createTextNode("Can you also guess the ability name?");
+        head.appendChild(node1);
+        para.appendChild(node2);
+        para2.appendChild(node3);
+        suggestionsContainer.appendChild(head);
+        suggestionsContainer.appendChild(para);
+        suggestionsContainer.appendChild(para2);
     } else newCell.style.backgroundColor = wrongColor;
+}
+
+//returns true or false if daily skill is eligible for gender unlock
+function checkForGenderUnlock() {
+    for (let indexGroup = 0; indexGroup < genderUnlockGroups.groups.length; indexGroup++) {
+        for (let indexClass = 0; indexClass < genderUnlockGroups.groups[indexGroup].groupID.length; indexClass++) {
+            if (dailyClass === genderUnlockGroups.groups[indexGroup].groupID[indexClass]) {
+                let firstClass = genderUnlockGroups.groups[indexGroup].groupID[0];
+                let secondClass = genderUnlockGroups.groups[indexGroup].groupID[1];
+                if (firstClass === dailyClass) {
+                    for (let indexSkill = 0; indexSkill < genderUnlockGroups.groups[indexGroup].skills.length; indexSkill++) {
+                        if (dailySkill === genderUnlockGroups.groups[indexGroup].skills[indexSkill][firstClass]) {
+                            alternateClass = secondClass;
+                            alternateSkill = genderUnlockGroups.groups[indexGroup].skills[indexSkill][secondClass];
+                            return true;
+                        }
+                    }
+                } else for (let indexSkill = 0; indexSkill < genderUnlockGroups.groups[indexGroup].skills.length; indexSkill++) {
+                    if (dailySkill === genderUnlockGroups.groups[indexGroup].skills[indexSkill][secondClass]) {
+                        alternateClass = firstClass;
+                        alternateSkill = genderUnlockGroups.groups[indexGroup].skills[indexSkill][firstClass];
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return false;
 }
 
 //event listener for pressing enter in input field -> calls the submit button function and removes input as a suggestion
@@ -153,7 +211,7 @@ inputContent.addEventListener("keypress", function(event) {
         removeItem(availableClasses, suggestions[0]);
 
         if (classGuessed) {
-            suggestions = abilityList[dailyClass].abilities.filter(name => name.toLowerCase().includes(query));
+            suggestions = abilityList[alternateClass].abilities.filter(name => name.toLowerCase().includes(query));
         }
 
         if (suggestions.length > 0) {
@@ -182,7 +240,7 @@ inputContent.addEventListener("input", function () {
 
     let suggestions = availableClasses.filter(name => name.toLowerCase().startsWith(query));
     if (classGuessed) {
-        suggestions = abilityList[dailyClass].abilities.filter(name => name.toLowerCase().includes(query));
+        suggestions = abilityList[alternateClass].abilities.filter(name => name.toLowerCase().includes(query));
     }
 
     if (query === '') {
@@ -190,13 +248,12 @@ inputContent.addEventListener("input", function () {
         return;
     }
 
-    let suggestionsContainer = document.getElementById("suggestions");
     suggestionsContainer.innerHTML = '';
 
 
     suggestions.forEach(suggestion => {
         let suggestionItem = document.createElement("div");
-        suggestionItem.classList.add('suggestion-item')
+        suggestionItem.classList.add('suggestion-item');
         suggestionItem.innerHTML = suggestion;
         suggestionsContainer.appendChild(suggestionItem);
     });
@@ -205,15 +262,14 @@ inputContent.addEventListener("input", function () {
 //function similar to getInput(); but only called after class is already guessed
 function getSkillInput() {
     //clearing suggestion container
-    let suggestionsContainer = document.getElementById("suggestions");
     suggestionsContainer.innerHTML = '';
     let value = inputContent.value;
 
-    if (value === dailySkill) {
+    if (value === alternateSkill) {
         alert("Correct !");
     } else {
         alert("Wrong! ):<");
-        alert("The skill to guess was: " + dailySkill);
+        alert("The skill to guess was: " + alternateSkill);
         alert("You entered: " + value);
     }
 
