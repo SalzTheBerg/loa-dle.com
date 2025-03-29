9fde3d23ec565bda4e3f0b00d121eb9949ca8a93
SalzTheBerg <silas.hittler@gmx.de>
2025-03-29 14:46:52 +0100

    Added core function for location guesser

--- location.js
+++ location.js
@@ -1,31 +1,63 @@
 let locationList = [];
 let availableContinents = [];
+let locationSpecifications = [];
 
 let dailyContinent;
 let dailyLocation;
+let locationsInArea;
+let dailyLocationImage;
 let dailyImage;
 
-const imageDiv = document.getElementById("guess_image");
+const imageDiv = document.getElementById("location_image");
+const inputContent = document.getElementById("input_guess");
 
-//fetches the location list and stores into locationList array
-fetch("locationList.json")
-    .then(response => response.json())
-    .then(data => {
-        locationList = data;
-        availableContinents = Object.keys(locationList);
-        availableContinents.sort();
-        console.log(availableContinents);
-        console.log(locationList[availableContinents[0]].locations);
-        loadImg();
+let centerX;
+let centerY;
+let randomSeed = Math.floor(Math.random() * 4);
+let originalScale;
+let currentScale;
+
+Promise.all([
+    fetch("locationList.json").then(response => response.json()),
+    fetch("locationSpecifications.json").then(response => response.json())
+])
+    .then(([locationsData, specificationsData]) => {
+    locationList = locationsData;
+    availableContinents = Object.keys(locationList);
+    availableContinents.sort();
+    locationSpecifications = specificationsData;
+    loadImg();
     })
-    .catch(error => console.error("Error loading location data:", error));
+.catch(error => console.error("Error loading data:", error));
 
 //loads the daily image randomly and adding it to the DOM
 function loadImg() {
-    dailyContinent = availableContinents[Math.floor(Math.random() * availableContinents.length)];
-    dailyLocation = locationList[dailyContinent].locations[Math.floor(Math.random() * locationList[dailyContinent].locations.length)]
-    alert("Still in progress, doesn't work yet");
+    dailyContinent = "Arthetine"//availableContinents[Math.floor(Math.random() * availableContinents.length)];
+    dailyLocation = "Origins of Stern"//locationList[dailyContinent].locations[Math.floor(Math.random() * locationList[dailyContinent].locations.length)]
+    locationsInArea = Object.keys(locationSpecifications[dailyLocation]);
+    dailyLocationImage = "Origins of Stern_1"//locationsInArea[Math.floor(Math.random() * locationsInArea.length)];
 
-    imageDiv.innerHTML = '<img src="Locations/' + dailyContinent + '_' + dailyLocation + '.webp" id="dailyLocation">';
+    imageDiv.innerHTML = '<img src="Locations/' + dailyContinent + '_' + dailyLocationImage + '.jpg" id="dailyLocation">';
+    centerX = -2000;//locationSpecifications[dailyLocation][dailyLocationImage].centerX[randomSeed];
+    centerY = -1800;//locationSpecifications[dailyLocation][dailyLocationImage].centerY[randomSeed];
+    originalScale = 7;//locationSpecifications[dailyLocation][dailyLocationImage].originalScale[randomSeed];
+    currentScale = originalScale;
     dailyImage = document.getElementById("dailyLocation");
+    dailyImage.style.transform = "translate(" + centerX + "px, " + centerY + "px) scale(" + originalScale + ")";
 }
+
+inputContent.addEventListener("keydown", function(event) {
+    if (event.keyCode == 13) {
+        if (currentScale > 1) {
+            currentScale = currentScale * 0.85;
+            if (currentScale < 1) {
+                currentScale = 1;
+            }
+
+            let newX = centerX * ((currentScale - 1) / (originalScale - 1));
+            let newY = centerY * ((currentScale - 1) / (originalScale - 1));
+
+            dailyImage.style.transform = "translate(" + newX + "px, " + newY + "px) scale(" + currentScale + ")";
+        }
+    }
+})
