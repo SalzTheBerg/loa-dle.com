328c39fff9cb1d78d38990c764b3778a89ddf7d1
SalzTheBerg <silas.hittler@gmx.de>
2025-04-11 20:55:04 +0200

    changed day

--- Modules/utilConsts.js
+++ Modules/utilConsts.js
@@ -2,7 +2,7 @@ export const correctColor = "rgb(96, 220, 0)";
 export const partialMatchColor = "rgb(225, 225, 0)";
 export const wrongColor = "rgb(238, 42, 0)";
 
-export const today = "2025-04-10"//new Date().toISOString().split('T')[0];
+export const today = "2025-04-09"//new Date().toISOString().split('T')[0];
 
 export const focusState = {
     focusActive: false,
